3d18d9c1fd2fc8f4b9d949984687071d46e5423e
Makpieter <pietermakowski@gmail.com>
2026-05-28 21:46:25 +0200

    creature movement fix

--- hero-common/src/main/java/pl/psi/creatures/CreatureStats.java
+++ hero-common/src/main/java/pl/psi/creatures/CreatureStats.java
@@ -5,22 +5,23 @@ import com.google.common.collect.Range;
 import lombok.*;
 
 /**
- * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
+ * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description.).
  */
 @RequiredArgsConstructor
 @Builder
 @Getter
-public class CreatureStats implements CreatureStatisticIf{
+public class CreatureStats implements CreatureStatisticIf {
     private final String name;
     private final int attack;
-
     private final int armor;
     private final int maxHp;
     private final int moveRange;
-    private final Range< Integer > damage;
+    private final Range<Integer> damage;
     private final int tier;
     private final String description;
     private final boolean isUpgraded;
+    @Builder.Default
+    private final MovementType movementType = MovementType.WALKING;
 
     public CreatureStats(CreatureStats other) {
         this.name = other.name;
@@ -28,10 +29,10 @@ public class CreatureStats implements CreatureStatisticIf{
         this.armor = other.armor;
         this.maxHp = other.maxHp;
         this.moveRange = other.moveRange;
-        // Tworzymy nowy obiekt Range, żeby było deep copy
         this.damage = Range.closed(other.damage.lowerEndpoint(), other.damage.upperEndpoint());
         this.tier = other.tier;
         this.description = other.description;
         this.isUpgraded = other.isUpgraded;
+        this.movementType = other.movementType;
     }
 }
