f027dfb00de169af50654f04b758ac9efa4d1ff3
Makpieter <pietermakowski@gmail.com>
2026-06-14 13:19:58 +0200

    bastion_castle_implementation

--- economy-model/src/main/java/pl/psi/hero/EconomyHero.java
+++ economy-model/src/main/java/pl/psi/hero/EconomyHero.java
@@ -17,6 +17,7 @@ import pl.psi.map.resources.Resources;
 
 public class EconomyHero implements PropertyChangeListener
 {
+    @Getter
     private final Fraction fraction;
     private final List< EconomyCreature > creatureList;
     @Getter
